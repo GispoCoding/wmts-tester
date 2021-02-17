6043f9afe234476aee8ba10eae83929e21c62ec2
Joonalai <joona@gispo.fi>
2021-02-17 13:33:50 +0200

    Adjust to another wmst server

--- main.js
+++ main.js
@@ -6,22 +6,10 @@ import View from 'ol/View';
 import {transformExtent} from 'ol/proj';
 import {getCenter} from 'ol/extent';
 import TileWMS from "ol/source/TileWMS";
-import {set} from "ol/transform";
 
 
 const url = process.env.WMS_URL
 const capabilities_url = `${url}?request=GetCapabilities&service=WMS&version=1.3.0`
-
-
-function threeHoursAgo() {
-    return new Date(Math.round(Date.now() / 3600000) * 3600000 - 3600000 * 3);
-}
-
-
-console.log(threeHoursAgo().toISOString());
-let date = new Date('2020-09-25T14:05:00Z');
-console.log(date.toISOString());
-
 
 const addMap = async () => {
     const dates = await fetch(capabilities_url)
@@ -31,14 +19,16 @@ const addMap = async () => {
             const xmlDoc = parser.parseFromString(txt, "text/xml");
 
             return xmlDoc.getElementsByTagName('Dimension')[0].childNodes[0].nodeValue
-                .split('/')
+                .split(',')
                 .filter(dim => dim.length > 4)
-                .map(dim => new Date(dim));
+                .map(dim => dim.trim())
+                .map(dim => new Date(`${dim}T14:05:00.000Z`));
         });
 
 
-    var extent = transformExtent([26.51500222, 9.61734492, 28.42005830, 11.14981886], 'EPSG:4326', 'EPSG:3857');
-    var startDate = dates[0];
+    const extent = transformExtent([26.9998170118793, 8.94756456080834, 29.832916841254, 10.8554812215646],
+        'EPSG:4326', 'EPSG:3857');
+    let startDate = dates[0];
 
     const layers = [
         new TileLayer({
@@ -52,17 +42,17 @@ const addMap = async () => {
                 params: {'LAYERS': process.env.LAYER, 'TIME': startDate.toISOString()},
             }),
         })];
-    let map = new Map({
+    const map = new Map({
         layers: layers,
         target: 'map',
         view: new View({
             center: getCenter(extent),
-            zoom: 10,
+            zoom: 9,
         }),
     });
 
     const updateInfo = () => {
-        var el = document.getElementById('info');
+        const el = document.getElementById('info');
         el.innerHTML = startDate.toISOString();
     }
 
@@ -74,11 +64,11 @@ const addMap = async () => {
     }
 
 
-    var previousBtn = document.getElementById('previous');
-    previousBtn.addEventListener('click', () => setTime(false), false);
+    document.getElementById('previous')
+        .addEventListener('click', () => setTime(false), false);
 
-    var nextBtn = document.getElementById('next');
-    nextBtn.addEventListener('click', () => setTime(true), false);
+    document.getElementById('next')
+        .addEventListener('click', () => setTime(true), false);
 
     updateInfo();
 }
